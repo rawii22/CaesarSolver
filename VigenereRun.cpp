957094dcf3cdc15263f51b604e6467d9aadd945c
jhthomas44 <77jamesthomas@gmail.com>
2023-02-21 23:12:12 -0600

    Vigenere Code Rewrite
    Rewrote the vigenere program to more easily allow future improvements.
    Made every function static.
    Added a function that automatically uses the new functions to completely solve a vigenere cipher.
    Changed period calculation to use a different method and return a list of possible periods.
    Changed decrypting to return a string instead of outputting, to do the whole text at once, and to keep extra characters, spaces, and letter capitalization.
    Changed the remove spaces function to remove all special characters also.
    Made a new function that returns the index of the largest value in an array.
    Made a function to calculate an alphabet's likely keys and removed the old function that stored translated alphabets.
    Made a function to find the most likely overall key from the calculated keys.
    Made a function to turn the key into a string.
    Modified the run file to work after the changes.

--- VigenereRun.cpp
+++ VigenereRun.cpp
@@ -5,20 +5,12 @@
 #include "VigenereSolver.h"
 
 int main(){
-    std::string encryptedText = "";
+    std::string encryptedText;
     while (encryptedText.empty())
     {
         std::cout << "\nPlease enter a ciphertext:\n";
         getline(std::cin, encryptedText);
     }
 
-    VigenereSolver vigenereSolver(encryptedText);
-    // If the results seem a little off, try writing the above line as:
-    // VigenereSolver vigenereSolver(encryptedText, 2);
-    // Increasing the second parameter at the end will force the solver to
-    // print out more possible solutions. It's likely the correct translation
-    // is nearby in the long list. However, remember, the number of printed
-    // results will be equal to <second parameter> ^ <the period of the key>.
-
-    vigenereSolver.printResults();
+    VigenereSolver::printSolveVigenere(encryptedText);
 }
--- VigenereSolver.cpp
+++ VigenereSolver.cpp
@@ -3,6 +3,49 @@
 #include <string>
 #include <cmath>
 
+// Does all steps for decrypting a vigenere cypher automatically
+// hasExtraInfo also returns the period and the key in the returned string
+std::string VigenereSolver::solveVigenere(std::string encryptedText, bool hasExtraInfo = false){
+    // Simplify text for calculating period and keys
+    std::string cleanEncryptedText = cleanText(encryptedText);
+    // Set a max period to check for
+    int maxPeriodChecked = sqrt(cleanEncryptedText.length()) + 1;
+    // Calculate the most likely periods
+    double* periodList = calculateIdealPeriod(cleanEncryptedText, maxPeriodChecked);
+    // Get most likely period
+    int period = getHighestValueIndex(periodList, maxPeriodChecked) + 1;
+    // Calculate the correlation frequencies for the alphabets
+    double** decodedAlphabetFrequencies = calculateKeys(cleanEncryptedText, period);
+    // Get most likely key
+    int* key = CalculateMostLikelyKey(decodedAlphabetFrequencies, period);
+    // Decrypt text
+    std::string decryptedText = "";
+
+    if (hasExtraInfo){
+        decryptedText += "\nYour period is calculated to be " + std::to_string(period) + "\n\n";
+        decryptedText += "Your key is " + convertKeyToString(key, period) + "\n\n";
+        decryptedText += "Decrypted Text:\n";
+    }
+
+    decryptedText += decodeVigenere(encryptedText, key, period);
+
+    // Delete arrays
+    delete[] periodList;
+    delete[] key;
+    for (int i = 0; i < period; i++){
+        delete[] decodedAlphabetFrequencies[i];
+    }
+    delete[] decodedAlphabetFrequencies;
+
+    return decryptedText;
+}
+
+// Prints the result of solveVigenere with extra info
+void VigenereSolver::printSolveVigenere(std::string encryptedText){
+    std::cout << solveVigenere(encryptedText, true);
+}
+
+
 // Index of Coincidence (IC) Calculator
 // This is used to calculate if the period of a key is valid. If this returns a value greater than 0.06,
 // then it is more likely that the period of the alphabet is one. If this is true for the majority of
@@ -10,14 +53,14 @@
 double VigenereSolver::ICCalculator(std::string text){
     int* freq = CaesarSolver::getFrequencyOfLetters(text);
     int totalLetters = 0;
-    for (int i = 0; i < 26; i++)
-    {
+
+    for (int i = 0; i < LANGUAGE_LETTER_COUNT; i++){
         totalLetters += freq[i];
     }
 
     double total = 0;
 
-    for (int i = 0; i < 26; i++){
+    for (int i = 0; i < LANGUAGE_LETTER_COUNT; i++){
         total += freq[i] * (freq[i] - 1);
     }
 
@@ -27,158 +70,151 @@ double VigenereSolver::ICCalculator(std::string text){
 }
 
 // Determines the most likely period using Index of Correlation (IC) Values
-void VigenereSolver::calculateIdealPeriod(){
-    double originalTextIC = ICCalculator(encryptedText);
-
-    PERIOD = compareICToPeriodTable(originalTextIC);
-    bool periodNotCorrect = true;
+double* VigenereSolver::calculateIdealPeriod(std::string cleanEncryptedText, int maxPeriodChecked){
     int numberOfAlphabetsSuggestingPeriodOfOne;
-    bool add = true;
-    int addBy = 1;
+    std::string* alphabets;
+    double* periodsList = new double[maxPeriodChecked];
 
 
-    while (periodNotCorrect){
-        if (alphabets != NULL){ // Make sure to de-allocate memory if alphabets has already been tested!
-            delete [] alphabets;
-        }
-
-        alphabets = splitAlphabets();
+    for (int periodBeingChecked = 1; periodBeingChecked <= maxPeriodChecked; periodBeingChecked++){
+        alphabets = splitAlphabets(cleanEncryptedText, periodBeingChecked);
         numberOfAlphabetsSuggestingPeriodOfOne = 0;
 
         // Counts the number of split alphabets that suggests a period of 1
-        for (int i = 0; i < PERIOD; i++){
-            if (compareICToPeriodTable(ICCalculator(alphabets[i])) == 1){
+        for (int i = 0; i < periodBeingChecked; i++){
+            if (ICCalculator(alphabets[i]) > VALUE_SUGGESTING_PERIOD_OF_ONE){
                 numberOfAlphabetsSuggestingPeriodOfOne++;
             }
         }
 
-        // If at least half of the alphabets suggest a period of 1, exit the loop
-        if (numberOfAlphabetsSuggestingPeriodOfOne >= PERIOD/2){
-            periodNotCorrect = false;
-        }
-        else{ // Otherwise periodically go through the adjacent periods and calcualte their values
-            if (add){
-                PERIOD += addBy;
-                add = false;
-            }
-            else {
-                PERIOD -= addBy;
-                add = true;
-            }
-            addBy += 1;
-        }
-        // If the calculations fail, set period to -1 and exit
-        if (PERIOD <= 0){
-            PERIOD = -1;
-            return;
-        }
+        delete[] alphabets;
+        periodsList[periodBeingChecked - 1] = (double) numberOfAlphabetsSuggestingPeriodOfOne / periodBeingChecked;
     }
 
-    std::cout << "Your period was calculated to be " << PERIOD << "\n\n";
+    return periodsList;
 }
 
-// Compares the IC value to all the values in the table and returns the period that it most closely correlates with
-int VigenereSolver::compareICToPeriodTable(double ICValue){
-    int mostLikelyPeriod = 1;
-    double mostLikelyIC = ENGLISH_LANGUAGE_IC_VALUES[0];
+// Returns a 2D array that has for each alphabet (number of alphabets = period)
+// an array that contains the corerelation freqeuncy for each possible shift
+double** VigenereSolver::calculateKeys(std::string cleanEncryptedText, int period){
+    double** decodedAlphabetFrequencies = new double*[period];
+    std::string* alphabets = splitAlphabets(cleanEncryptedText, period);
 
-    for (int i = 0; i < NUMBER_OF_IC_VALUES; i++){
-        if (std::abs(ENGLISH_LANGUAGE_IC_VALUES[i] - ICValue) < std::abs(mostLikelyIC - ICValue)){
-            mostLikelyPeriod = i + 1;
-            mostLikelyIC = ENGLISH_LANGUAGE_IC_VALUES[i];
-        }
+    for (int i = 0; i < period; i++){
+        decodedAlphabetFrequencies[i] = CaesarSolver::getCorrelationOfFrequencies(alphabets[i]);
     }
 
-    return mostLikelyPeriod;
+    delete[] alphabets;
+    return decodedAlphabetFrequencies;
 }
 
-// This function finishes attempting to decode the ciphertext using the calculated period.
-void VigenereSolver::printResults(){
-    std::string** possibleDecodedAlphabets = new std::string*[PERIOD];
-    for (int i = 0; i < PERIOD; i++){
-        possibleDecodedAlphabets[i] = new std::string[TOP_NUM];
+// Returns the most likely key from the information gathered from the alphabet frequencies
+int* VigenereSolver::CalculateMostLikelyKey(double** decodedAlphabetFrequencies, int period){
+    int* key = new int[period];
+
+    for (int i = 0; i < period; i++){
+        key[i] = getHighestValueIndex(decodedAlphabetFrequencies[i], LANGUAGE_LETTER_COUNT);
     }
 
-    // For each alphabet, translate all of them using their most favorable shift values.
-    std::cout << "\nMost likely key:\n";
-    for (int i = 0; i < PERIOD; i++){
-        double* correlationFrequencies = CaesarSolver::getCorrelationOfFrequencies(alphabets[i]);
-        int* topFive = CaesarSolver::getTopShifts(correlationFrequencies, TOP_NUM);
+    return key;
+}
 
-        for (int j = 0; j < TOP_NUM; j++){
-            possibleDecodedAlphabets[i][j] = CaesarSolver::unshift(alphabets[i], topFive[j]);
+// Takes the key as an integer array and returns it as a string of letters
+std::string VigenereSolver::convertKeyToString(int* key, int period){
+    std::string stringKey = "";
+
+    for (int i = 0; i < period; i++){
+        stringKey += CaesarSolver::convertNumberToLetter(key[i]);
+    }
+
+    return stringKey;
+}
+
+// decrypts the provided string based off the period and key supplied to it
+std::string VigenereSolver::decodeVigenere(std::string encryptedText, int* key, int period){
+    std::string decryptedText = "";
+    int currentAlphabet = 0;
+
+    for (char character : encryptedText){
+        // If the character isn't a letter, just save the character and continue
+        if (character < 65 || (character > 90 && character < 97) || character > 122){
+            decryptedText += character;
+            continue;
         }
-        std::cout << CaesarSolver::convertNumberToLetter(topFive[0]);
+        // Check if lowercase letter 
+        if (character >= 97 && character <= 122){
+            character -= key[currentAlphabet];
+            // Check for underflow
+            if (character < 97){
+                character += 26;
+            }   
+        }
+        // Check if uppercase letter
+        else {
+            character -= key[currentAlphabet];
+            // Check for underflow
+            if (character < 65){
+                character += 26;
+            }
+        }
+        decryptedText += character;
 
-        delete[] correlationFrequencies;
-        delete[] topFive;
+        // Increment which alphabet is currently being used
+        currentAlphabet++;
+        if (currentAlphabet == period){
+            currentAlphabet = 0;
+        }
     }
 
-    std::cout << "\n\n";
-
-    // for (int i = 0; i < PERIOD; i++){
-    //     std::cout << possibleDecodedAlphabets[i][0];
-    //     std::cout << "\n";
-    // }
-
-    // std::cout << "\n";
-
-    std::cout << "Most likely plain text:\n";
-    weaveAlphabets(possibleDecodedAlphabets, removeSpaces(encryptedText).length());
-    
-    for (int i = 0; i < PERIOD; i++){
-        delete[] possibleDecodedAlphabets[i];
-    }
-    delete[] possibleDecodedAlphabets;
+    return decryptedText;
 }
 
 // Splits the encrypted text into a number of alphabets equal to the calculated period and returns an array of the alphabets
-std::string* VigenereSolver::splitAlphabets(){
-    std::string cleanedText = removeSpaces(encryptedText);
-    std::string* alphabets = new std::string[PERIOD];
+std::string* VigenereSolver::splitAlphabets(std::string cleanEncryptedText, int period){
+    std::string* alphabets = new std::string[period];
     int periodTracker = 0;
 
-    for (int i = 0; i < PERIOD; i++){
+    for (int i = 0; i < period; i++){
         alphabets[i] = "";
     }
-    for (char i : cleanedText){
+    for (char i : cleanEncryptedText){
         alphabets[periodTracker] += i;
         periodTracker++;
         //Resets counter if exceed period
-        periodTracker %= PERIOD;
+        periodTracker %= period;
     }
     return alphabets;
 }
 
-// Takes a string and returns the string with all spaces removed
-std::string VigenereSolver::removeSpaces(std::string textWithSpaces){
-    std::string textWithoutSpaces = "";
+// Removes all non letter characters and capitalizes lowercase letters
+std::string VigenereSolver::cleanText(std::string rawEncryptedText){
+    std::string cleanText = "";
 
-    for (char i : textWithSpaces){
+    for (char i : rawEncryptedText){
         if (i != ' '){
-            textWithoutSpaces += i;
-        }
-    }
-
-    return textWithoutSpaces;
-}
-
-// Takes an 2D array of alphabets and their specified top most likely solutions along with the size of the encrypted text's letters
-// Then weaves them together in all permutations to all possible decryptions with the listed parameters
-// TOP_NUM and PERIOD determine the number of results printed. (Number of results = TOP_NUM ^ PERIOD)
-void VigenereSolver::weaveAlphabets(std::string** possibleDecodeAlphabets, int encryptedTextLengthWithoutSpaces){
-    int numberOfCharactersPrinted = 0;
-
-    for (int i = 0; i < pow(TOP_NUM, PERIOD); i++){ // For each most likely translation for each individual alphabet...
-        for (int j = 0; j < encryptedTextLengthWithoutSpaces / PERIOD + 1; j++){ // ...print out a letter...
-            for (int k = 0; k < PERIOD; k++){ // ...from each row, and keep cycling through the rows in order to interlace the letters back into their original order.
-                std::cout << possibleDecodeAlphabets[k][(i / (int) pow(TOP_NUM, k)) % TOP_NUM][j]; // This chooses which permutation (of all the most likely translations for EACH alphabet) to reconstruct and print out.
-                numberOfCharactersPrinted++;
-                if (numberOfCharactersPrinted == encryptedTextLengthWithoutSpaces){ // Don't print more letters than exist in the original text.
-                    break;
-                }
+            if (i >= 97 && i <= 122){ // Capitalize the lower letters
+                cleanText += toupper(i);
+            }
+            else if (i >= 65 && i <= 90){ // Include capital letters
+                cleanText += i;
             }
         }
-        std::cout << "\n\n";
     }
+
+    return cleanText;
+}
+
+// Returns the index of the highest value in an array
+int VigenereSolver::getHighestValueIndex(double* listOfValues, int sizeOfList){
+    int highestValueIndex = 0;
+    double highestValue = listOfValues[0];;
+
+    for (int i = 0; i < sizeOfList; i++){
+        if (listOfValues[i] > highestValue){
+            highestValue = listOfValues[i];
+            highestValueIndex = i;
+        }
+    }
+
+    return highestValueIndex;    
 }
