12bb2e405983dfade8de7ebd2627ce7664770656
jhthomas44 <77jamesthomas@gmail.com>
2023-03-11 12:08:22 -0600

    Added file writing to vigenere encryption
    Added a prompt to VigenereRun that asks if the user wants to write the encrypted text to a file.

--- VigenereRun.cpp
+++ VigenereRun.cpp
@@ -14,7 +14,17 @@ int main(){
     userInput = DataManipulation::getIntegerInput(1, 2);
 
     if (userInput == 1){
-        std::cout << "\n" << Vigenere::encrypt();
+        std::string encryptedText = Vigenere::encrypt();
+        std::cout << "\n" << encryptedText;
+        
+        std::cout << "\n\nWould you like to write the text to a file? Y/[N]\n";
+        userInput = DataManipulation::getLetterInput();
+        if (userInput == 'Y' || userInput == 'y'){
+            std::string fileName;
+            std::cout << "\nWhat file would you like to output to?\n> ";
+            getline(std::cin, fileName);
+            DataManipulation::writeStringToFile(fileName, encryptedText);
+        }
     }
     else {
         Vigenere::solveVigenere(DataManipulation::getUserInput("Please enter the file name:", "Please enter a ciphertext:"));
